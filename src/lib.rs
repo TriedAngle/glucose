adec7268fb3538b577144a2bba7407fe7abefeab
Sebastian <Sebastian@Strobl.net>
2020-09-12 22:35:37 +0200

    Add: vectors macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+mod vec;
+mod mat;
+
 #[cfg(test)]
 mod tests {
+    use super::vec::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let test_vec_1 = Vec2::new([0.5, 1.0]);
+        println!("{:?}", test_vec_1);
     }
 }
---
+++ src/vec.rs
@@ -0,0 +1,26 @@
+macro_rules! vectors {
+    ($($n:ident => [$t:ty; $d:expr]),+) => {
+        $(
+            #[derive(Clone, Copy, Debug, Default)]
+            #[repr(C)]
+            pub struct $n {
+                pub data: [$t; $d]
+            }
+
+            impl $n {
+                #[inline]
+                pub const fn new(data: [$t; $d]) -> Self {
+                    Self {
+                        data,
+                    }
+                }
+            }
+        )+
+    }
+}
+
+vectors! {
+  Vec2 => [f32; 2],
+  Vec3 => [f32; 3],
+  Vec4 => [f32; 4]
+}

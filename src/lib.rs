d198d274c8f939d5bddd16702a1e4041741b3733
Sebastian <Sebastian@Strobl.net>
2020-09-13 13:36:35 +0200

    Add: remaining operators

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,14 @@ mod vec;
 #[cfg(test)]
 mod tests {
     use super::vec::*;
+    use std::ops::{Neg, Not};
+
     #[test]
     fn create_vectors() {
         let vec_1 = Vec3::new([0.0, 1.0, 2.0]);
         let vec_2 = Vec3::unit_y();
 
+        assert_eq!(vec_1.data, [0.0, 1.0, 2.0]);
         assert_eq!(vec_2.data, [0.0, 1.0, 0.0]);
     }
 
@@ -28,5 +31,17 @@ mod tests {
         let dot = vec_1.dot(&vec_2);
 
         assert_eq!(dot, 12.0);
+    }
+
+    #[test]
+    fn scale_vectors() {
+        let mut vec_1 = Vec3::new([1.0, 3.0, 1.0]);
+        let vec_2 = Vec3::new([2.0, 6.0, 2.0]);
+        vec_1 = 2.0 * vec_1;
+
+        let vec_3 = vec_2.neg();
+
+        assert_eq!(vec_1, vec_2);
+        assert_eq!(vec_3.data, [-2.0, -6.0, -2.0])
     }
 }
--- src/vec.rs
+++ src/vec.rs
@@ -1,13 +1,13 @@
 #![allow(dead_code)]
 
-use std::ops::Add;
+use std::ops::{Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div, DivAssign, Neg};
 use paste::paste;
 
 #[macro_export]
 macro_rules! vectors {
     ($($n:ident => [$t:ty; $d:expr]),+) => {
         $(
-            #[derive(Clone, Copy, Debug, Default)]
+            #[derive(Clone, Copy, Debug, Default, PartialEq)]
             #[repr(C)]
             pub struct $n {
                 pub data: [$t; $d]
@@ -44,20 +44,149 @@ macro_rules! vectors {
 
             impl Add for $n {
                 type Output = Self;
-
+                #[inline]
                 fn add(self, other: $n) -> Self {
                     let mut data = [<$t>::default(); $d];
-                    for i in 0..($d - 1) {
+                    for i in 0..$d {
                          data[i] = self.data[i] + other.data[i];
                     }
                     Self::new(data)
+                }
+            }
+
+            impl AddAssign for $n {
+                #[inline]
+                fn add_assign(&mut self, other: $n) {
+                    for i in 0..$d {
+                        self.data[i] += other.data[i];
+                    }
+                }
+            }
+
+            impl Sub for $n {
+                type Output = Self;
+                #[inline]
+                fn sub(self, other: $n) -> Self {
+                    let mut data = [<$t>::default(); $d];
+                    for i in 0..$d {
+                         data[i] = self.data[i] - other.data[i];
+                    }
+                    Self::new(data)
+                }
+            }
+
+            impl SubAssign for $n {
+                #[inline]
+                fn sub_assign(&mut self, other: $n) {
+                    for i in 0..$d {
+                        self.data[i] -= other.data[i];
+                    }
+                }
+            }
+
+            // Mul is maybe a little bit weird to have for vectors but it may come in handy?
+            // maybe replace this with the dot or cross product?
+            impl Mul for $n {
+                type Output = Self;
+                #[inline]
+                fn mul(self, other: $n) -> Self {
+                    let mut data = [<$t>::default(); $d];
+                    for i in 0..$d {
+                         data[i] = self.data[i] * other.data[i];
+                    }
+                    Self::new(data)
+                }
+            }
+
+            /// vec' := vec1 *= vec2
+            impl MulAssign for $n {
+                #[inline]
+                fn mul_assign(&mut self, other: $n) {
+                    for i in 0..$d {
+                        self.data[i] *= other.data[i];
+                    }
+                }
+            }
+
+            /// vec' = vec * scalar
+            impl Mul<$t> for $n {
+                type Output = Self;
+                #[inline]
+                fn mul(self, other: $t) -> Self {
+                    let mut data = [<$t>::default(); $d];
+                    for i in 0..$d {
+                         data[i] = self.data[i] * other;
+                    }
+                    Self::new(data)
+                }
+            }
+
+            /// vec' = scalar * vec
+            impl Mul<$n> for $t {
+                type Output = $n;
+                #[inline]
+                fn mul(self, other: $n) -> $n {
+                    let mut data = [<$t>::default(); $d];
+                    for i in 0..$d {
+                         data[i] = other.data[i] * self;
+                    }
+                    $n::new(data)
+                }
+            }
+
+            /// vec' := vec * scalar
+            impl MulAssign<$t> for $n {
+                #[inline]
+                fn mul_assign(&mut self, other: $t) {
+                    for i in 0..$d {
+                        self.data[i] *= other;
+                    }
+                }
+            }
+
+            // Div is maybe a little bit weird to have for vectors but it may come in handy?
+            impl Div for $n {
+                type Output = Self;
+                #[inline]
+                fn div(self, other: $n) -> Self {
+                    let mut data = [<$t>::default(); $d];
+                    for i in 0..$d {
+                         data[i] = self.data[i] / other.data[i];
+                    }
+                    Self::new(data)
+                }
+            }
+
+            impl DivAssign for $n {
+                #[inline]
+                fn div_assign(&mut self, other: $n) {
+                    for i in 0..$d {
+                        self.data[i] /= other.data[i];
+                    }
+                }
+            }
+
+            impl DivAssign<$t> for $n {
+                #[inline]
+                fn div_assign(&mut self, other: $t) {
+                    for i in 0..$d {
+                        self.data[i] /= other;
+                    }
+                }
+            }
+
+            impl Neg for $n {
+                type Output = $n;
+                #[inline]
+                fn neg(self) -> $n {
+                    self * -1 as $t
                 }
             }
         )+
     }
 }
 
-// This macro makes working with vectors easier, by adding letters
+// This macro makes working with vectors easier by adding letters
 #[macro_export]
 macro_rules! letters_for_vectors {
     ($($n:ident => [$($c:expr => $d:ident),+]  {$t:ty}),+) => {
@@ -89,6 +218,7 @@ macro_rules! letters_for_vectors {
 }
 
 // define vectors and implement basics
+// ($($n:ident => [$t:ty; $d:expr]),+)
 vectors! {
     Vec1 => [f32; 1],
     Vec2 => [f32; 2],

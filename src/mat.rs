f8d8cf11956344b5d56ab7397718c945bf13989d
Sebastian <Sebastian@Strobl.net>
2021-02-15 21:02:20 +0100

    Swap: Matrix<T, N, M> -> Matrix<T, M, N>

--- src/mat.rs
+++ src/mat.rs
@@ -2,17 +2,17 @@
 
 use crate::traits::MathComponent;
 use crate::vec::Vector;
-use std::ops::{Add, Sub};
+use std::ops::{Add, Mul, Sub};
 
 pub type SquareMatrix<T, const N: usize> = Matrix<T, { N }, { N }>;
 
 #[derive(Debug)]
-pub struct Matrix<T, const N: usize, const M: usize> {
-    data: [[T; M]; N],
+pub struct Matrix<T, const M: usize, const N: usize> {
+    data: [[T; N]; M],
 }
 
-impl<T, const N: usize, const M: usize> Matrix<T, { N }, { M }> {
-    pub fn new(data: [[T; M]; N]) -> Self {
+impl<T, const M: usize, const N: usize> Matrix<T, { M }, { N }> {
+    pub fn new(data: [[T; N]; M]) -> Self {
         Self { data }
     }
 
@@ -21,10 +21,10 @@ impl<T, const N: usize, const M: usize> Matrix<T, { N }, { M }> {
     }
 }
 
-impl<T: MathComponent<T> + Copy, const N: usize, const M: usize> Matrix<T, { N }, { M }> {
+impl<T: MathComponent<T> + Copy, const M: usize, const N: usize> Matrix<T, { M }, { N }> {
     pub fn zero() -> Self {
         Self {
-            data: [[<T>::zero(); M]; N],
+            data: [[<T>::zero(); N]; M],
         }
     }
 }
@@ -39,13 +39,13 @@ impl<T: MathComponent<T> + Copy, const N: usize> SquareMatrix<T, { N }> {
     }
 }
 
-impl<T: MathComponent<T> + Copy, const N: usize, const M: usize> Add for Matrix<T, { N }, { M }> {
+impl<T: MathComponent<T> + Copy, const M: usize, const N: usize> Add for Matrix<T, { M }, { N }> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let mut data = [[<T>::default(); M]; N];
-        for i in 0..N {
-            for j in 0..M {
+        let mut data = [[<T>::default(); N]; M];
+        for i in 0..M {
+            for j in 0..N {
                 data[i][j] = self.data[i][j] + rhs.data[i][j];
             }
         }
@@ -53,13 +53,13 @@ impl<T: MathComponent<T> + Copy, const N: usize, const M: usize> Add for Matrix<
     }
 }
 
-impl<T: MathComponent<T> + Copy, const N: usize, const M: usize> Sub for Matrix<T, { N }, { M }> {
+impl<T: MathComponent<T> + Copy, const M: usize, const N: usize> Sub for Matrix<T, { M }, { N }> {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        let mut data = [[<T>::default(); M]; N];
-        for i in 0..N {
-            for j in 0..M {
+        let mut data = [[<T>::default(); N]; M];
+        for i in 0..M {
+            for j in 0..N {
                 data[i][j] = self.data[i][j] - rhs.data[i][j];
             }
         }
@@ -67,8 +67,20 @@ impl<T: MathComponent<T> + Copy, const N: usize, const M: usize> Sub for Matrix<
     }
 }
 
-impl<T: Copy, const N: usize, const M: usize> Matrix<T, { N }, { M }> {
-    pub fn to_vectors(&self) -> Vec<Vector<T, { M }>> {
+impl<T, const M: usize, const N: usize, const P: usize> Mul<Matrix<T, { N }, { P }>>
+    for Matrix<T, { M }, { N }>
+where
+    T: MathComponent<T> + Copy,
+{
+    type Output = Matrix<T, { M }, { P }>;
+
+    fn mul(self, rhs: Matrix<T, { N }, { P }>) -> Self::Output {
+        unimplemented!()
+    }
+}
+
+impl<T: Copy, const M: usize, const N: usize> Matrix<T, { M }, { N }> {
+    pub fn to_vectors(&self) -> Vec<Vector<T, { N }>> {
         self.data.iter().map(|col| Vector::new(*col)).collect()
     }
 }
